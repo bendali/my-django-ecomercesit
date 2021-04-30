783ba451a2b46c39a94e1a2c01f61a899d6e3c7f
bendali7194@gmail.com <bendali7194@gmail.com>
2021-04-30 06:16:19 +0100

    My Django Ecomsite app ,add Model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.FloatField(default=0.0)
+    discount_price = models.FloatField(default=0.0)
+    category = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.CharField(max_length = 300)
+    
